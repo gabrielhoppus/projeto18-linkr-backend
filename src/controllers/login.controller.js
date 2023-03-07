f4e684bdec167442eafa37064f7c71fabba6ca19
Thiago Saytson <thiagosaytson@poli.ufrj.br>
2023-03-07 16:54:36 -0300

    [feature] sign-up and sign-in routes

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { loginRepository } from "../repositories/login.repository.js";
 
 export async function signUp(req, res) {
@@ -14,8 +15,20 @@ export async function signUp(req, res) {
 
 export async function signIn(req, res) {
     const { user } = res.locals;
-    try {
 
+    try {
+        if (!user.token) {
+            const token = jwt.sign(
+                {
+                    id: user.id,
+                    email: user.email
+                },
+                process.env.SECRET_JWT,
+                { expiresIn: 60 * 60 * 24 * 30 });
+            await loginRepository.updateUserToken(user, token);
+            return res.status(200).send({ token });
+        }
+        return res.status(200).send({ token: user.token });
     } catch (error) {
         console.log(error);
         res.status(500).send(error);
--- src/repositories/login.repository.js
+++ src/repositories/login.repository.js
@@ -1,18 +1,24 @@
-import { connectionDB } from "../database/database.js";
+import { db } from "../config/database.connection.js";
 
 function findUser(email) {
-    return connectionDB.query(`SELECT * FROM users 
+    return db.query(`SELECT * FROM users 
     WHERE email=$1;`, [email]);
 
 }
 
 function insertUser({username, email, password, picture}) {
-    return connectionDB.query(`INSERT INTO users 
-    (email, username, password, picture)
+    return db.query(`INSERT INTO users 
+    (username, email, password, picture)
     VALUES ($1, $2, $3, $4);`, [username, email, password, picture]);
+}
+
+function updateUserToken(user, token) {
+    return db.query(`UPDATE users SET token=$1 WHERE id=$2;`,
+        [token, user.id]);
 }
 
 export const loginRepository = {
     findUser,
-    insertUser
+    insertUser,
+    updateUserToken
 }
