1ec2b63cae77a83bacfc4b53fd6e69941d7af129
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-03-10 16:43:03 -0300

    fix: post listings

--- src/middlewares/token.middleware.js
+++ src/middlewares/token.middleware.js
@@ -5,32 +5,31 @@ import { findUser } from "../repositories/user.repository.js";
 dotenv.config();
 
 export async function validateToken(req, res, next) {
-  const { authorization } = req.headers;
-  const parts = authorization?.split(" ");
+    const { authorization } = req.headers;
+    const parts = authorization?.split(" ");
 
-  try {
-    if (!authorization || parts.length !== 2)
-      return res.status(401).send("Invalid Token");
-    const [schema, token] = parts;
+    try {
+        if (!authorization || parts.length !== 2)
+            return res.status(401).send("Invalid Token");
+        const [schema, token] = parts;
 
-    if (schema !== "Bearer")
-      return res.status(401).send("Invalid Token Schema");
+        if (schema !== "Bearer")
+            return res.status(401).send("Invalid Token Schema");
 
-    jwt.verify(token, process.env.SECRET_KEY, async (error, decoded) => {
-      if (error) {
-        return res.status(401).send("Invalid Token Error");
-      }
+        jwt.verify(token, process.env.SECRET_KEY, async (error, decoded) => {
+            if (error) {
+                return res.status(401).send("Invalid Token Error");
+            }
 
-      const user = (await findUser(decoded.id)).rows[0];
+            const user = (await findUser(decoded.id)).rows[0];
 
-      if (!user || !user.id) return res.status(401).send("Invalid User Token");
-      req.user_id = decoded.id;
-      console.log(req.user_id);
-      res.locals.session = token;
+            if (!user || !user.id) return res.status(401).send("Invalid User Token");
+            req.user_id = decoded.id;
+            res.locals.session = token;
 
-      next();
-    });
-  } catch (error) {
-    return res.status(500).send(error);
-  }
+            next();
+        });
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
