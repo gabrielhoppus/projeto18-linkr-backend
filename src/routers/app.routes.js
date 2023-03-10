e7843a25ece834a7146c13dea7f68f9e03aa1413
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-03-10 20:17:18 -0300

    add, remove, and list likes

--- src/routers/app.routes.js
+++ src/routers/app.routes.js
@@ -3,13 +3,14 @@ import loginRoutes from './login.routes.js';
 import userRoutes from './user.routes.js';
 import postRoutes from "./post.routes.js"
 import hashTagRoutes from './hashtag.routes.js'
+import likeRoutes from './like.routes.js'
 
 
 
 const router = Router();
 
 
-router.use([loginRoutes, userRoutes , postRoutes ,hashTagRoutes]);
+router.use([loginRoutes, userRoutes , postRoutes ,hashTagRoutes, likeRoutes]);
 
 
 export default router;
